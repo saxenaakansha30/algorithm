c9fabb75ad13e451ef87e8c143e4e4e227fdccbd
saxenaakansha30 <saxenaakansha30@gmail.com>
2021-11-12 08:23:31 +0530

    Program for counting set bits

---
+++ GeeksForGeeks/BitwiseOperations/countSetBits.cpp
@@ -0,0 +1,33 @@
+//
+//  countSetBits.cpp
+//  GeeksForGeeks
+//
+//  Created by Akansha.Saxena on 12/11/21.
+//
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int getSetBitCount(int n) {
+        int count = 0, i=1;
+        while (i <= n) {
+            if ((n&i) != 0) {
+                count++;
+            }
+            i = i<<1;
+        }
+        return count;
+    }
+};
+
+int main() {
+    int n;
+    Solution obj;
+    cout<<"Enter a number for which you want to see set bit count: ";
+    cin>>n;
+    cout<<obj.getSetBitCount(n);
+    cout<<endl;
+    return 0;
+}
