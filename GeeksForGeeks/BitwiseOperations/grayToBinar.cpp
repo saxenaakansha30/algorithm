c3969587c57afa3ba1993deba465b4e3b645a253
saxenaakansha30 <saxenaakansha30@gmail.com>
2021-11-21 20:43:23 +0530

    Gray code to binary conversion

--- GeeksForGeeks/BitwiseOperations/grayToBinar.cpp
+++ GeeksForGeeks/BitwiseOperations/grayToBinar.cpp
@@ -5,4 +5,25 @@
 //  Created by Akansha.Saxena on 21/11/21.
 //
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int grayToBinary(int n) {
+        int res = 0;
+        while(n!=0) {
+            res = res^n;
+            n=n>>1;
+        }
+        return res;
+    }
+};
+
+int main() {
+    int n=4;
+    Solution obj;
+    cout<<obj.grayToBinary(n);
+    cout<<endl;
+    return 0;
+}
