6a7a94fa2c149e7e8429a3852d666cc27a80a1b5
saxenaakansha30 <saxenaakansha30@gmail.com>
2021-11-21 08:02:58 +0530

    Chceck largest consecutives of 1's

--- GeeksForGeeks/BitwiseOperations/LargestConsecutive1s.cpp
+++ GeeksForGeeks/BitwiseOperations/LargestConsecutive1s.cpp
@@ -5,4 +5,27 @@
 //  Created by Akansha.Saxena on 21/11/21.
 //
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int getLargestConsecutives1sCount(int n) {
+        int counter = 0;
+        while(n!=0) {
+            n = n&(n>>1);
+            counter++;
+        }
+        return counter;
+    }
+};
+
+int main() {
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    Solution obj;
+    cout<<obj.getLargestConsecutives1sCount(n);
+    cout<<endl;
+    return 0;
+}
